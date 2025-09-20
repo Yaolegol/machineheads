60d18e5c44a961f4f148f25315bf22499e547b0c
Yaolegol <m160160@yandex.ru>
2025-09-21 04:18:07 +0700

    Рефакторинг

--- src/screens/PostList/index.tsx
+++ src/screens/PostList/index.tsx
@@ -1,8 +1,9 @@
-import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {PostList} from "@/modules/post/components/PostList";
 import {useDispatch} from "react-redux";
 import {getPostListRequest} from "@/modules/post/store/actions";
-import {useHistory, useLocation, useParams} from 'react-router-dom';
+import {useHistory, useLocation} from 'react-router-dom';
+import {PAGE_QUERY_PARAM_NAME} from "@/modules/post/constants";
 
 const PostListScreen: React.FC = () => {
     const dispatch = useDispatch();
@@ -12,7 +13,7 @@ const PostListScreen: React.FC = () => {
 
     const handlePageChange = useCallback((page: number) => {
         const searchParams = new URLSearchParams(location.search);
-        searchParams.set('page', String(page));
+        searchParams.set(PAGE_QUERY_PARAM_NAME, String(page));
 
         history.push({
             pathname: location.pathname,
@@ -22,7 +23,7 @@ const PostListScreen: React.FC = () => {
 
     useEffect(() => {
         const searchParams = new URLSearchParams(location.search);
-        const page = searchParams.get('page');
+        const page = searchParams.get(PAGE_QUERY_PARAM_NAME);
 
         if (page) {
             setPage(Number(page))
