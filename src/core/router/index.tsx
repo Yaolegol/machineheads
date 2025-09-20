a026303a0eff21ebe0ad7c977c89374f261f86e7
Yaolegol <m160160@yandex.ru>
2025-09-21 02:34:25 +0700

    Доработка компонента первичной проверки токена авторизации

--- src/core/router/index.tsx
+++ src/core/router/index.tsx
@@ -3,9 +3,9 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import {PrivateRoute} from "@/core/router/components/PrivateRoute";
 import {history} from '@/core/store';
-import PostListScreen from "@/screens/PostList";
 
 const AuthScreen = React.lazy(() => import('@/screens/Auth'));
+const PostListScreen = React.lazy(() => import('@/screens/PostList'));
 
 export const AppRouter: FC = () => {
     return (
